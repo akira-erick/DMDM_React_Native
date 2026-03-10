5f01fb14b9b95f20d6b11c762167a3af40778d5f
akira-erick <kajitani.motta@gmail.com>
2026-03-10 20:25:19 -0300

    feat(app): creating first screen

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, ImageBackground } from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+
+import { styles } from './app-styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={require('./assets/earth.png')}
+          style={styles.background}
+          resizeMode='cover'
+          >
+            <Button title="Press me" onPress={() => alert("Button pressed")} />
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app-styles.js
+++ app-styles.js
@@ -4,11 +4,14 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   text: {
     fontSize: 20,
     color: "black",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
