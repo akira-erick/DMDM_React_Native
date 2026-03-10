e9fe729f8765f021a81abdae67e094c32d163e3f
akira-erick <kajitani.motta@gmail.com>
2026-03-10 19:58:15 -0300

    feat(database-service): adicionando função para pegar perguntas

--- database-service/loadData.ts
+++ database-service/loadData.ts
@@ -1,3 +1,11 @@
+import * as SQLite from 'expo-sqlite';
+
 export async function loadData() {
-    
+    const db = SQLite.openDatabaseSync('database.db')
+
+    const result = await db.execAsync(`
+        SELECT * FROM questions;
+    `);
+
+    return result;
 }
